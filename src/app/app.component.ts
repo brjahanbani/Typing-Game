448812c74615ac3973978442069701ce58c3990a
Babak <babak.jahanbani@yahoo.com>
2022-02-09 18:13:54 +0330

    handling user input
    {{}} & (input)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
-
-  
+  randomText: string = lorem.sentence();
+  onChangeInput(text: string) {
+    console.log(text);
+  }
 }
